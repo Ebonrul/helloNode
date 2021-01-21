87671f5630f5cfc58fd5daab9e8dc3a326aa4f6c
Ebonrul <54614412+Ebonrul@users.noreply.github.com>
2021-01-21 01:34:43 -0500

    roadblock
    Generating an error when trying to parse the local environment variable

--- server.js
+++ server.js
@@ -1,4 +1,11 @@
+/* eslint-disable no-console */
 /* eslint-disable strict */
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').parse();
+}
+
+
 const express = require('express');
 const app = express();
 const expressLayouts = require('express-ejs-layouts');
@@ -10,6 +17,14 @@ app.set('views', __dirname + '/views');
 app.set('layout', 'layouts/layout');
 app.use(expressLayouts);
 app.use(express.static('public'));
+
+const mongoose = require('mongoose');
+const { parse } = require('dotenv/types');
+mongoose.connect(process.env.DATABASE_URL, {
+  useNewUrlParser: true});
+const db = mongoose.connection;
+db.on('error', error => console.error(error));
+db.once('open', () => console.log('Connected to Mongoose'));
 
 app.use('/', indexRouter);
 
