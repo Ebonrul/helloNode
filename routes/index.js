62564ecf663b1bd2dc41ed02e7ef469cda96cca0
Ebonrul <54614412+Ebonrul@users.noreply.github.com>
2021-01-21 00:37:37 -0500

    initial commit
    This is the beginning of the project, showing a functional "Hello World" server setup.

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+/* eslint-disable strict */
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+module.exports = router;
